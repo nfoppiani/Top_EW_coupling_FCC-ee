88ebda802825e9e4289cca5247133def1ee06cfd
nfoppiani <nicolo.foppiani@gmail.com>
2015-07-29 13:33:02 +0200

    Added the jobCopy files

--- 01_tt_one_lepton/jobs_copy_marlin/jobCopy_yyxyev.sh
+++ 01_tt_one_lepton/jobs_copy_marlin/jobCopy_yyxyev.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-eosDir=/store/cmst3/user/pjanot/ttbar365Wizard/yyxyev/
-castorDir=/castor/cern.ch/grid/ilc/prod/clic/365gev/yyveyx_o/ILD/REC/00005251/000/
+#eosDir=/store/cmst3/user/nfoppian/ttbar365Wizard/yyxyev/
+castorDir=/castor/cern.ch/grid/ilc/prod/clic/365gev/yyxyev_o/ILD/REC/00005248/000/
+
+#661 files
 
 for ((job=1;job<3;job++));
   do
   echo "JOB "$job
-  slcioFile="yyveyx_o_rec_5251_"${job}".slcio"
-  eosFile="yyveyx_o_"${job}".slcio"
+  slcioFile="yyxyev_o_rec_5248_"${job}".slcio"
+  #eosFile="yyxyev_o_"${job}".slcio"
   fileToStage=$castorDir$slcioFile
   nsls $fileToStage
   stager_qry -M $fileToStage
   stager_get -M $fileToStage
   # To copy the staged files on eos, issue the command
-  # eos cp -r root://castorcms////castor/cern.ch/grid/ilc/prod/clic/365gev/yyveyx_o/ILD/REC/00005251/000/ /store/cmst3/user/pjanot/ttbar365Wizard/yyxyev/
+  # eos cp -r root://castorcms////castor/cern.ch/grid/ilc/prod/clic/365gev/yyxyev_o/ILD/REC/00005248/000/ /eos/cms/store/cmst3/user/nfoppian/ttbar365Wizard/yyxyev/
 done
