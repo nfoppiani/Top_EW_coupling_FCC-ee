227910e82cf90cc1dd3c31741aea2d4c7acda87c
nfoppiani <nicolo.foppiani@gmail.com>
2015-08-03 15:53:41 +0200

    Added two new scripts.

--- 01_tt_one_lepton/jobs_copy_marlin/jobCopy_yyxylv_1_2nd_ver.sh
+++ 01_tt_one_lepton/jobs_copy_marlin/jobCopy_yyxylv_1_2nd_ver.sh
@@ -2,14 +2,14 @@
 
 # this program copies the yyvlyx_o_rec_5233_<number>.slcio files contained in castorDir in eos directory
 # this program is issued prompting 'sh castorCopy.sh'
-eosDir=/eos/cms/store/cmst3/user/tpajero/ttbar365Wizard/yyvlyx/000/
-castorDir=/castor/cern.ch/grid/ilc/prod/clic/365gev/yyvlyx_o/ILD/REC/00005233/000/
+eosDir=/eos/cms/store/cmst3/user/nfoppian/ttbar365Wizard/yyxylv/000/
+castorDir=/castor/cern.ch/grid/ilc/prod/clic/365gev/yyxylv_o/ILD/REC/00005352/000/
 
-for ((job=77;job<78;job++));
+for ((job=610;job<612;job++));
   do 
   echo "JOB "$job
   # writes JOB and the job number as the job is run
-  slcioFile="yyvlyx_o_rec_5233_"${job}".slcio"
+  slcioFile="yyxylv_o_rec_5352_"${job}".slcio"
   # assigns the progressive name of slcioFile
   fileToCopy=$castorDir$slcioFile
 xrdcp root://castorcms//$fileToCopy root://eoscms/$eosDir
