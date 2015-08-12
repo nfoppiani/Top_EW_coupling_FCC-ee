648362f7cbcfd06e7f98e656fd4fc036d02aa24c
nfoppiani <nicolo.foppiani@gmail.com>
2015-08-12 19:06:41 +0200

    New macro added

--- 01_tt_one_lepton/whizard/classes/backgroundSignalMuonPlot.py
+++ 01_tt_one_lepton/whizard/classes/backgroundSignalMuonPlot.py
@@ -22,11 +22,11 @@ hAnglePartial1=TH1F("Angle to the closest charged particle signal","Angle to the
 hAnglePartial2=TH1F("Angle to the closest charged particle bg","Angle to the closest charged particle bg",100,0.,1.)
 
 
-#hPtJetAngleChar=TH2F("Pt to the closest Jet, Angle to the closest charged particle","Pt to the closest Jet, Angle to the closest charged particle",100,0.,100.,100,0.,1.)
+hPtJetAngleChar=TH2F("Pt to the closest Jet, Angle to the closest charged particle","Pt to the closest Jet, Angle to the closest charged particle",100,0.,100.,100,0.,1.)
 
-#hPtJetAngleChar=TH2F("Pt to the closest Jet, Angle to the closest charged particle","Pt to the closest Jet, Angle to the closest charged particle",100,0.,100.,100,0.,1.)
+hPtJetAngleCharPartial1=TH2F("Pt to the closest Jet, Angle to the closest charged particle SIGNAL","Pt to the closest Jet, Angle to the closest charged particle SIGNAL",100,0.,100.,100,0.,1.)
 
-#hPtJetAngleChar=TH2F("Pt to the closest Jet, Angle to the closest charged particle","Pt to the closest Jet, Angle to the closest charged particle",100,0.,100.,100,0.,1.)
+hPtJetAngleCharPartial2=TH2F("Pt to the closest Jet, Angle to the closest charged particle BG","Pt to the closest Jet, Angle to the closest charged particle BG",100,0.,100.,100,0.,1.)
 
 Pt=0.
 Angle=0.
@@ -52,8 +52,9 @@ for event in tree:
 		hPtPartial1.Fill(Pt)
 		
 		Angle=rcParticles[MatchNum].angleToClosestCharge(rcParticles)
-		hAngleClosestCharge.Fill(Angle)
+		hAnglePartial1.Fill(Angle)
 		
+		hPtJetAngleCharPartial1.Fill(Pt,Angle)
 		
 		for part in rcParticles:
 			if part.type ==13 and part.num!=MatchNum:
@@ -61,7 +62,9 @@ for event in tree:
 				hPtPartial2.Fill(Pt)
 				
 				Angle=part.angleToClosestCharge(rcParticles)
-				hAngleClosestCharge.Fill(Angle)
+				hAnglePartial2.Fill(Angle)
+				
+				hPtJetAngleCharPartial2.Fill(Pt,Angle)
 
 hPtPartial1.SetLineColor(2) #set the red fill color				
 #hPtClosestJet.Add(hPtPartial1)
@@ -69,5 +72,22 @@ hPtPartial1.SetLineColor(2) #set the red fill color
 hPtPartial2.SetLineColor(3) #set the green fill color
 #hPtClosestJet.Add(hPtPartial2)
 
-hPtPartial1.Draw()
-hPtPartial2.Draw("same")
+
+
+hAnglePartial1.SetLineColor(2) #set the red fill color				
+#hAngleClosestCharge.Add(hPtPartial1)
+
+hAnglePartial2.SetLineColor(3) #set the green fill color
+#hAngleClosestCharge.Add(hPtPartial2)
+
+
+
+hPtJetAngleCharPartial1.SetLineColor(2) #set the red fill color				
+#hPtJetAngleChar.Add(hPtJetAngleCharPartial1)
+
+hPtJetAngleCharPartial2.SetLineColor(3) #set the green fill color
+#hPtJetAngleChar.Add(hPtJetAngleCharPartial2)
+
+
+hPtJetAngleCharPartial1.Draw("Lego")
+hPtJetAngleCharPartial2.Draw("same")
